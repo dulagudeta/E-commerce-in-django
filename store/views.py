d04a1bddedafc378ab5948ca34e15b834a4688f2
Dula Gudeta <dulagudeta22@gmail.com>
2025-08-07 20:33:20 +0300

    updateItem view logic

--- store/views.py
+++ store/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Product, Order, OrderItem, ShippingAddress
 from django.http import JsonResponse
+import json
 
-# Create your views here.
+from .models import * 
+
 def store(request):
 	products = Product.objects.all()
 	context = {'products':products}
@@ -18,6 +19,7 @@ def cart(request):
 		#Create empty cart for now for non-logged in user
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0}
+
 	context = {'items':items, 'order':order}
 	return render(request, 'store/cart.html', context)
 
@@ -35,4 +37,26 @@ def checkout(request):
 	return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
 	return JsonResponse('Item was added', safe=False)
